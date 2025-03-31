b8e4cede85ea5683bcb33085bc4cc3d1c3833beb
RosarioCaltabianoCPG <rosario.caltabiano@cloudpathology.eu>
2025-04-01 01:01:34 +0200

    host 0.0.0.0 on python app

--- api/main.py
+++ api/main.py
@@ -101,4 +101,4 @@ def search_esami():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Crea le tabelle al primo avvio
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
